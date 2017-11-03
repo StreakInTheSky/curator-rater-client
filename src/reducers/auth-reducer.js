c8c93c4e332c1bc759ecb0f623fa9d21bfb3eccc
StreakInTheSky <streakinthesky@gmail.com>
2017-11-03 05:31:44 -0700

    remove console.log

--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -13,7 +13,6 @@ export default function(state = initialState, action) {
   if (action.type === SET_AUTH_TOKEN) {
     return Object.assign({}, state, { authToken: action.authToken })
   } else if (action.type === SET_CURRENT_USER) {
-    console.log('setting current user:', action.user)
     return Object.assign({}, state, { currentUser: action.user })
   } else if (action.type === FETCH_CURRENT_USER_ERROR) {
     console.error(action.error)
