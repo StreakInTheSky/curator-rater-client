051f0b34fec1c03add86fbe2b21bc15bde5b376b
StreakInTheSky <streakinthesky@gmail.com>
2017-11-05 02:04:33 -0800

    fix follow buttons styles.

--- src/components/profile/user-listing.js
+++ src/components/profile/user-listing.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function UserListing(props) {
   const followButton = props.following.indexOf(props.userId) >= 0
-  ? <span className="mock-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>unfollow</span>
+  ? <span className="mock-button follow-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>unfollow</span>
   : <span className="mock-button follow-button" onClick={()=>props.follow(props.userId)}>follow</span>
 
   return (
