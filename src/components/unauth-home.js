1185c007543f730d9b07dbeade044919850d7139
StreakInTheSky <streakinthesky@gmail.com>
2017-11-10 07:54:18 -0800

    Create hero on landing page

--- src/components/unauth-home.js
+++ src/components/unauth-home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import Gallery from './unauth-gallery'
 import * as actions from '../actions/gallery'
+
+import './home.css'
 
 export class UnauthHome extends React.Component {
   componentWillMount() {
@@ -20,7 +23,11 @@ export class UnauthHome extends React.Component {
 
     return (
       <div>
-        <p className="page-description">Whether you're a photographer, designer, tastemaker, or just want to share your ideas. Curator-rater is there to help you find your voice an audience. Login or sign up and start curating new galleries or rating them.</p>
+        <div className="page-hero">
+          <p className="tagline">Share your vision with us.</p>
+          <p className="sub-tagline">Whether you're a photographer, designer, tastemaker, or just want to share your ideas. Curator-rater is there to help you find your audience.</p>
+          <Link to="/signup" className="mock-button">Sign Up</Link>
+        </div>
         <main>
           {galleries}
         </main>
