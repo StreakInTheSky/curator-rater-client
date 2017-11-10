158179a8d712546924c24a2046d43189bb78d022
StreakInTheSky <streakinthesky@gmail.com>
2017-11-10 06:47:00 -0800

    alter follow buttons and style user listings

--- src/components/profile/follow-button.js
+++ src/components/profile/follow-button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FollowButton(props) {
   if (props.following) {
-    return <span className="mock-button follow-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>Unfollow</span>
+    return <span className="mock-button follow-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>following</span>
   }
-  return <span className="mock-button follow-button" onClick={()=>props.follow(props.userId)}>Follow</span>
+  return <span className="mock-button follow-button" onClick={()=>props.follow(props.userId)}>follow</span>
 }
--- src/components/profile/user-listing.js
+++ src/components/profile/user-listing.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function UserListing(props) {
   const followButton = props.following.indexOf(props.userId) >= 0 || props.followed
-  ? <span className="mock-button follow-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>unfollow</span>
+  ? <span className="mock-button follow-button unfollow-button" onClick={()=>props.unfollow(props.userId)}>following</span>
   : <span className="mock-button follow-button" onClick={()=>props.follow(props.userId)}>follow</span>
 
   return (
-      <li>
+      <li className="user-list-listing">
         <Link to={`/${props.username}`} >{props.username}</Link>
         {props.myId === props.userId ? null : followButton}
       </li>
