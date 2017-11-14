cd0028df8bbfae9a84a7b11bc457efa79eb43a04
StreakInTheSky <streakinthesky@gmail.com>
2017-11-13 23:47:50 -0800

    add hero to unauthed home

--- src/components/unauth-home.js
+++ src/components/unauth-home.js
@@ -25,7 +25,7 @@ export class UnauthHome extends React.Component {
       <div>
         <div className="page-hero">
           <p className="tagline">Share your vision with us.</p>
-          <p className="sub-tagline">Whether you're a photographer, designer, tastemaker, or just want to share your ideas. Curator-rater is there to help you find your audience.</p>
+          <p className="sub-tagline">Whether you're a photographer, designer, tastemaker, or just want to share your ideas. Curator-rater is here to help you find your audience.</p>
           <Link to="/signup" className="mock-button">Sign Up</Link>
         </div>
         <main>
