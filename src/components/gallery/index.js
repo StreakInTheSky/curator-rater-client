12b19a2466543a951ef4100e0648fc64e9a692a0
StreakInTheSky <streakinthesky@gmail.com>
2017-11-18 05:52:04 -0800

    fix bug with stars when changing favorites.

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -17,16 +17,38 @@ export class Gallery extends React.Component {
     this.state = {
       currentImage: this.props.gallery.images[0],
       showMoreInfo: false,
-      favorited: false
+      favorited: false,
+      favoritedAmount: 0
     }
 
     this.viewImage = this.viewImage.bind(this)
     this.vote = this.vote.bind(this)
   }
 
+  componentDidMount() {
+    this.checkFavorited(this.props.currentUser.favorites)
+    this.setState({favoritedAmount: this.props.gallery.favorited_by.length})
+  }
+
   componentWillReceiveProps(nextProps) {
     if(this.props.gallery._id !== nextProps.gallery._id){
       this.setState({currentImage: nextProps.gallery.images[0]})
+    }
+    this.checkFavorited(nextProps.currentUser.favorites)
+  }
+
+  checkFavorited(userFavorites) {
+    const galleryId = this.props.gallery._id ? this.props.gallery._id : this.props.gallery.id
+    const favoriteIds = userFavorites.map(gallery => {
+      if(gallery._id) {
+        return gallery._id
+      }
+      return gallery
+    })
+    if (favoriteIds.indexOf(galleryId) !== -1) {
+      this.setState({favorited: true})
+    } else {
+      this.setState({favorited: false})
     }
   }
 
@@ -38,6 +60,7 @@ export class Gallery extends React.Component {
     if (this.props.ownGallery) {
       return null;
     }
+    this.setState({favoritedAmount: this.props.gallery.favorited_by.length + 1})
     this.props.dispatch(actions.addFavoriteGallery(galleryId, this.props.currentUser.id))
   }
 
@@ -45,6 +68,7 @@ export class Gallery extends React.Component {
     if (this.props.ownGallery) {
       return null;
     }
+    this.setState({favoritedAmount: this.props.gallery.favorited_by.length - 1})
     this.props.dispatch(actions.removeFavoriteGallery(galleryId, this.props.currentUser.id))
   }
 
@@ -60,15 +84,16 @@ export class Gallery extends React.Component {
     if (!this.props.currentUser) {
       return <p>Loading gallery...</p>
     } else {
-      const {title, description, user, images, _id, id, favorited_by} = this.props.gallery
-      const favoriteIds = this.props.currentUser.favorites.map(gallery => gallery._id)
+      // console.log('current user data:', this.props.currentUser)
+      const {title, description, user, images, _id, id} = this.props.gallery
+      // const favoriteIds = this.props.currentUser.favorites.map(gallery => gallery._id)
       const galleryId = id ? id : _id
 
-      const favoriteStar = favoriteIds.indexOf(galleryId) !== -1
+      const favoriteStar = this.state.favorited
       ? <span className="favorite-star favorited" title="remove from favorites" onClick={()=>this.removeFavorite(galleryId)} ><i className="fa fa-star" aria-hidden="true"></i></span>
       : <span className="favorite-star" title="Add to favorites" onClick={()=>this.addFavorite(galleryId)} ><i className="fa fa-star-o" aria-hidden="true"></i></span>
 
-      const favoritedInfo = <div className="favorited-info">{favoriteStar} {favorited_by.length}</div>
+      const favoritedInfo = <div className="favorited-info">{favoriteStar} {this.state.favoritedAmount}</div>
 
       const galleryUsername = <p className="gallery-user"><Link to={`/${user.username}`}>@{user.username}</Link></p>
       const displayUsername = this.props.profileGallery ? null : galleryUsername
